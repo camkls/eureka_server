e8e09779ea31f052c1aaa7c357566182911a1464
camkls <cam_kls@163.com>
2020-04-26 16:10:12 +0800

    add show interface

---
+++ src/main/java/cn/sz/lb/Show.java
@@ -0,0 +1,18 @@
+package cn.sz.lb;
+
+/**
+ * @author lb
+ * @version 1.0
+ * @date 2020/4/26 15:55
+ **/
+public interface Show {
+
+    /**
+     * 查找信息
+     *
+     * @param username
+     * @param pwd
+     * @return
+     */
+    public String showInfo(String username, String pwd);
+}
